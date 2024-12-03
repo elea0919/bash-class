3032c297140c150afce90064af2f058aa15fd074
EC2 Default User <ec2-user@ip-172-31-19-186.ec2.internal>
2024-12-03 02:54:28 +0000

    bash script files

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo yum install git -y && sudo yum install tree -y && sudo yum install httpd
---
+++ test1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+my_name=Elea
+fav_food=plov
+fav_season=spring
+
+
+echo "Hello my name is $my_name, and my favorite food is $fav_food, if your favorite season is $fav_season, let is get along"
